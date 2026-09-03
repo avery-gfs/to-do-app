17ee1a9dee997b6d0e0a922be86e4f65b464f37c
Avery <anortonsmith@germantownfriends.org>
2026-09-03 13:35:19 -0400

    Initial version

---
+++ app.py
@@ -0,0 +1,29 @@
+import os
+
+done = []
+undone = []
+
+while True:
+  os.system("clear")
+  print("tasks:\n")
+
+  for task in undone:
+    print("[ ]", task)
+
+  for task in done:
+    print("[x]", task)
+
+  task = input("\ntask name: ")
+
+  if task in done:
+    done.remove(task)
+
+  elif task in undone:
+    done.append(task)
+    undone.remove(task)
+
+  else:
+    undone.append(task)
+
+  done.sort()
+  undone.sort()
